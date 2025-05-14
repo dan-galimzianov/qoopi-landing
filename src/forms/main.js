6825709e18c299701b2abb51e416d38b212ae0fe
Daniil Galimzianov <dan.galimzianov@gmail.com>
2025-05-14 13:25:55 +0400

    Forms fetch

--- src/forms/main.js
+++ src/forms/main.js
@@ -14,27 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 const input = item.querySelector('.form__item-input');
                 const required = item.getAttribute('data-form-validation-required') === 'true';
                 const name = input.getAttribute('name');
-
+                const isCheckbox = input.type === 'checkbox';
                 if (required) {
                     if (input.value.trim() === '') {
                         errors[name] = 'Поле обязательно для заполнения';
                     }
                 }
 
-                if (required && input.type === 'checkbox') {
+                if (required && isCheckbox) {
                     if (!input.checked) {
                         errors[name] = 'Поле обязательно для заполнения';
                     }
                 }
 
-                const isUrl = item.getAttribute('data-form-validation-is-url') === 'true';
                 const isEmail = item.getAttribute('data-form-validation-is-email') === 'true';
-
-                if (isUrl) {
-                    if (!input.value.match(/^https?:\/\/[^\s]+$/)) {
-                        errors[name] = 'Неверный формат ссылки';
-                    }
-                }
 
                 if (isEmail) {
                     if (!input.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -43,21 +36,26 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
 
                 const isMultiselect = item.getAttribute('data-form-validation-is-multiselect') === 'true';
-                values[name] = isMultiselect ? input.value.split(',') : input.value;
+                values[name] = isMultiselect ? input.value.split(',').filter(Boolean) : input.value;
+
+                if (isCheckbox) {
+                    values[name] = input.checked;
+                }
             });
 
-            if (Object.keys(errors).length > 0) {
+            const isInvalid = Object.keys(errors).length > 0;
+            
+            if (isInvalid) {
                 message = 'Пожалуйста, заполните все обязательные поля';
-            }
-
-            if (message) {
-                alert(message);
             }
 
             const serverUrl = form.getAttribute('data-server-url');
 
             fetch(serverUrl, {
                 method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(values)
             })
         });
